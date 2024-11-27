867dba57252240019f47e214ca539fd81bdf79ff
Kooreis <kooreis.official@gmail.com>
2024-11-27 12:19:27 -0600

    In this update, the user is prompted to enter the elements of the matrix. A nested for loop is used to iterate through the matrix and assign the user's input to each element. The scanner is then closed.

--- solution.java
+++ solution.java
@@ -1,8 +1,7 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the size of the square matrix:");
-        int n = scanner.nextInt();
-        int[][] matrix = new int[n][n];
+System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        scanner.close();

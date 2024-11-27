b765fe88270c6f28a8259b5d316f66072fcccd3c
Kooreis <kooreis.official@gmail.com>
2024-11-27 12:19:27 -0600

    Created the main program class and the main method. Initialized a 2D array and called the IsMagicSquare method.

--- solution.cs
+++ solution.cs
@@ -1,2 +1,7 @@
-using System;
-using System.Linq;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix = new int[,] { { 2, 7, 6 }, { 9, 5, 1 }, { 4, 3, 8 } };
+        Console.WriteLine(IsMagicSquare(matrix));
+    }
